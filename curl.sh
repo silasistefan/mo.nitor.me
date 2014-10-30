f8faa51a17fac367c42be0e4db61ac49e27f68dc
root <root@gw.stefan.cf>
2014-10-30 21:46:42 +0000

    Forgot about the URLs without custom Host header.

--- curl.sh
+++ curl.sh
@@ -4,6 +4,12 @@ host=`echo $1 | sed 's/^.*\.\([a-z\-]*\.[a-z]*\).*/\1/'`
 fhost=`echo $1 | awk -F\/ '{print $3;}'`
 req=`echo $1 | sed 's/^.*\/\([a-z]*\.[a-z]*\)$/\1/'`
 
+if [ ! -z $3 ]; then
+    url=https://$3/${req}
+else
+    url=$1
+fi
+
 x1=1000
 
 for ns in `dig -4 +time=1 +tries=2 NS $host | grep IN | grep -v ";" | awk '{print $5;}'`; do
@@ -22,7 +28,7 @@ if [ $# -gt 0 ]; then
         --max-time 5 \
         -w "DNS_TIME=${dns}&CURL_DNS=%{time_namelookup}&SSL_TIME=%{time_appconnect}&TRANSFER_TIME=%{time_total}&HTTP_CODE=%{http_code}&DOWNLOAD_SIZE=%{size_download}\n" \
         --max-redirs 5 \
-        -skLo /dev/null https://$3/${req}
+        -skLo /dev/null ${url}
 else
     echo "Error: Bad usage."
 fi

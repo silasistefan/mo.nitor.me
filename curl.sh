e2de74a8466b49af28f1801f382b33949fb31d14
root <root@gw.stefan.cf>
2014-11-02 10:25:29 +0000

    - increased the timeouts, since the negative values look like a DNS/CURL timeout, happening mainly in China and South America

--- curl.sh
+++ curl.sh
@@ -12,9 +12,9 @@ fi
 
 x1=1000
 
-for ns in `dig -4 +time=1 +tries=2 NS $host | grep IN | grep -v ";" | awk '{print $5;}'`; do
-    x1=`dig -4 +norecurse +time=1 +tries=2 $fhost @$ns | grep "Query time" | awk '{print $4;}'`
-    ip=`dig -4 +norecurse +time=1 +tries=2 +short $fhost @$ns`
+for ns in `dig -4 +time=3 +tries=2 NS $host | grep IN | grep -v ";" | awk '{print $5;}'`; do
+    x1=`dig -4 +norecurse +time=3 +tries=2 $fhost @$ns | grep "Query time" | awk '{print $4;}'`
+    ip=`dig -4 +norecurse +time=3 +tries=2 +short $fhost @$ns`
     if [ $x1 -lt 1000 ]; then break; fi
 done
 
@@ -24,8 +24,8 @@ dns=`printf %.3f $(echo "${x1}/1000" | bc -l)`
 if [ $# -gt 0 ]; then
     curl -4 \
         -A "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36" \
-        --connect-timeout 3 \
-        --max-time 5 \
+        --connect-timeout 5 \
+        --max-time 7 \
         -w "DNS_TIME=${dns}&CURL_DNS=%{time_namelookup}&SSL_TIME=%{time_appconnect}&TRANSFER_TIME=%{time_total}&HTTP_CODE=%{http_code}&DOWNLOAD_SIZE=%{size_download}\n" \
         --max-redirs 5 \
         -skLo /dev/null ${url}
